193bb2fb303c330e52c8d3204e82c0d92adde6f3
Andrew Kim <jeesoo2002@gmail.com>
2016-06-05 14:53:46 -0700

    practice 20

--- practice20/answer.js
+++ practice20/answer.js
@@ -2,15 +2,17 @@
  * Created by andrewkim on 5/30/16.
  */
 var input_array = [5,3,2,5,1];
+var output_array = [];
+
 function randomize_list(input_array) {
     var numCap = input_array.length;
     for(var i = 0; i < numCap; i++) {
-        var output_array = [];
-        var randomI = Math.floor((Math.random()*numCap));   //generate random num between 1 and 10
-        console.log("numCap: " + numCap);
+        var randomI = Math.floor((Math.random()*numCap-1));   //generate random num between 1 and random num
         console.log("random num: " + randomI);
-        console.log("input element: " + input_array.pop());
-        output_array[randomI] = input_array.pop();
+        console.log("input array: " + input_array);
+        var temp = input_array.pop();
+        output_array.splice(randomI,0,temp);
+        console.log("output array: " + output_array);
     }
     return output_array;
 }
